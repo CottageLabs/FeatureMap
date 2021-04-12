a1871de55c0c1c040983498d78a6b75b9a5d3e9c
Richard Jones <richard@cottagelabs.com>
2021-04-12 09:51:39 +0100

    make links out to source location in csv and html views

--- featuremap/csvserialiser.py
+++ featuremap/csvserialiser.py
@@ -7,6 +7,7 @@ class CSVSerialiser(Serialiser):
     """
     ~~CSV:Serialiser~~
     ~~-> Analysis:Model ~~
+    ~~-> Config:Config ~~
     """
     def serialise(self, data):
 
@@ -18,8 +19,11 @@ class CSVSerialiser(Serialiser):
             for k, relation in data.relations.items():
                 for rel, targets in relation.refs.items():
                     for target, occurrences in targets.items():
-                        refs = "; ".join([o["file"] + "#L" + str(o["line"]) for o in occurrences])
-                        writer.writerow([k, rel, target, refs])
+                        refs = [o["file"] + "#L" + str(o["line"]) for o in occurrences]
+                        urls = []
+                        if self.global_config.get("source_url") is not None:
+                            urls = [self.global_config.get("source_url") + "/" + f for f in refs]
+                        writer.writerow([k, rel, target, "; ".join(refs), "; ".join(urls)])
 
         filelist = os.path.join(container, "files.csv")
         with open(filelist, "w", encoding="utf-8") as o:
@@ -32,8 +36,11 @@ class CSVSerialiser(Serialiser):
         with open(entities, "w", encoding="utf-8") as o:
             writer = csv.writer(o)
             for k, v in data.entities.items():
-                refs = "; ".join([o["file"] + "#L" + str(o["line"]) for o in v])
-                writer.writerow([k, refs])
+                refs = [o["file"] + "#L" + str(o["line"]) for o in v]
+                urls = []
+                if self.global_config.get("source_url") is not None:
+                    urls = [self.global_config.get("source_url") + "/" + f for f in refs]
+                writer.writerow([k, "; ".join(refs), "; ".join(urls)])
 
         no_downstream = os.path.join(container, "no-downstream.csv")
         with open(no_downstream, "w", encoding="utf-8") as o:
