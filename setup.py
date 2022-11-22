8eb84126e6adac04536dc8b9d0128ec0b9bf6ee6
Steve Eardley <steve@eardley.xyz>
2022-11-22 10:43:53 +0000

    use setup.py rather than requirements.txt

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup, find_packages
-import sys
 
 # ~~Setup:Core~~
-
 setup(
-    name = 'featuremap',
-    version = '0.1.1',
-    packages = find_packages(),
-    install_requires = [],
-    url = 'http://cottagelabs.com/',
-    author = 'Cottage Labs',
-    author_email = 'richard@cottagelabs.com',
-    description = 'For annotating text files with features and their connections to other features.',
-    license = 'Apache2',
-    classifiers = [],
-    entry_points = {
+    name='featuremap',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        "lark==0.11.1",
+        "jinja2==2.11.3",
+        "pyyaml==5.4.1",
+        "markupsafe==2.0.1"
+    ],
+    url='https://cottagelabs.com/',
+    author='Cottage Labs',
+    author_email='richard@cottagelabs.com',
+    description='For annotating text files with features and their connections to other features.',
+    license='Apache2',
+    classifiers=[],
+    entry_points={
         'console_scripts': [
             # ~~-> CLI:Entrypoint ~~
             'featuremap=featuremap.cli:main',
